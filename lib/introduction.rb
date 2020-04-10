410a12e045e8ed75be46dc56dfbfb44eb987a32b
Yannick <ydjoumbi@gmail.com>
2020-04-10 20:36:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction (name,language)
   puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end
 
-introduction (yannick, ruby)
+introduction ('yannick', 'ruby')
   
 
 
