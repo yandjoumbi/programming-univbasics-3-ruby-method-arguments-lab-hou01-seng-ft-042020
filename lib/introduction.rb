41ae27f585f489577f41567912ff51f3ff3e55d9
Yannick <ydjoumbi@gmail.com>
2020-04-10 20:36:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,11 +3,11 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-def introduction (name)
-  puts "Hi, my name is #{name}."
+def introduction (name,language)
+  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end
 
-introduction (yannick)
+introduction (yannick, ruby)
   
 
 
